6a08db8f17d1f4245994f23354ba6d91c19234dc
fpresenza <francisco.presenza@gmail.com>
2024-03-21 11:53:24 -0300

    add comments

--- bls12_381_derive_pubkey/src/main.rs
+++ bls12_381_derive_pubkey/src/main.rs
@@ -3,13 +3,17 @@ use lambdaworks_math::{
         traits::IsEllipticCurve,
         short_weierstrass::curves::bls12_381::curve::BLS12381Curve
     },
-    cyclic_group::IsGroup};
+    cyclic_group::IsGroup
+};
 
 fn main() {
-    // parse secret key into an integer
+    // parse secret key into an unsigned integer
     let seckey = u64::from_str_radix("6C616D6264617370", 16).unwrap();
 
+    // group generator
     let g = BLS12381Curve::generator();
+
+    // compose 'g' with itself 'seckey' times
     let pubkey = g.operate_with_self(seckey);
     println!("{:?}", pubkey);
 }

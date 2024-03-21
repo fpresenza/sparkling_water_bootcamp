e9f81fab57253b286d2a9a52e92c45ab614b1de7
fpresenza <francisco.presenza@gmail.com>
2024-03-21 00:18:04 -0300

    derive public key in projective coordinates

--- bls12_381_derive_pubkey/src/main.rs
+++ bls12_381_derive_pubkey/src/main.rs
@@ -1,3 +1,15 @@
+use lambdaworks_math::{
+    elliptic_curve::{
+        traits::IsEllipticCurve,
+        short_weierstrass::{curves::bls12_381::curve::BLS12381Curve, traits::IsShortWeierstrass}
+    },
+    cyclic_group::IsGroup};
+
 fn main() {
-    println!("Hello, world!");
+    // parse secret key into an integer
+    let seckey = u64::from_str_radix("6C616D6264617370", 16).unwrap();
+
+    let g = BLS12381Curve::generator();
+    let privkey = g.operate_with_self(seckey);
+    println!("{:?}", privkey);
 }

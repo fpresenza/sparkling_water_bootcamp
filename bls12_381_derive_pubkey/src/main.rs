9bd050271cabe5e1d5ca87f10aed07705f229937
fpresenza <francisco.presenza@gmail.com>
2024-03-21 13:30:41 -0300

    add prints

--- bls12_381_derive_pubkey/src/main.rs
+++ bls12_381_derive_pubkey/src/main.rs
@@ -18,5 +18,7 @@ fn main() {
 
     // get affine (uncompressed) coordinates as a hexstring
     println!("public key x-affine coordinate: {:?}", pubkey.to_affine().x().value().to_hex());
+    // prints: public key x-affine coordinate: "773B526CAB2D1FEC900E6AD51E75BBE8EF6395D02F2FBFC84A1B09118FA4121A6EA675532E89A7B43AF5B9FFF9FE7F8"
     println!("public key y-affine coordinate: {:?}", pubkey.to_affine().y().value().to_hex());
+    // prints: public key y-affine coordinate: "117B6D42F9AB470E2719A696C954502FA963DFC9F0085ED1B419AFD0F205B039A66E824073D68B07F23707C3547FE979"
 }

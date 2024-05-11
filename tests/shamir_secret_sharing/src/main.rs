179971722a1bec4ede73b51fd39884e66b535c35
fpresenza <francisco.presenza@gmail.com>
2024-05-11 20:22:15 -0300

    shamir secret sharing in practice

--- tests/shamir_secret_sharing/src/main.rs
+++ tests/shamir_secret_sharing/src/main.rs
@@ -1,3 +1,57 @@
+use rand::prelude::*;
+use lambdaworks_math::{
+    field::fields::u64_prime_field::U64FieldElement,
+    polynomial::Polynomial
+};
+
+const MODULUS: u64 = 173;
+type FE = U64FieldElement<MODULUS>;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut rng = rand::thread_rng();
+    
+    // Define secret
+    let secret = FE::new(150);
+    println!("Secret is: {}", secret.value());
+    println!("");
+    
+    // Protocol Parameters
+    let n_shares = 5;
+    let n_suff   = 3;
+
+    // Create coefficient vector. First coeff is the secret.
+    let mut coeffs = vec![secret];
+
+    // Append the random coefficients in the range 1..n_suff-1
+    coeffs.append(
+        &mut
+        (1..=n_suff-1)
+        .map(|_| FE::new(rng.gen_range(0..MODULUS)))
+        .collect()
+    );
+
+    // Construct polynomial
+    let poly = Polynomial::new(&coeffs);
+
+    let shares: Vec<FE> = (1..=n_shares)
+        .map(|i| poly.evaluate(&FE::new(i)))
+        .collect();
+    println!("The shares to distribute are:");
+    for i in 1..=n_shares {
+        println!("({}, {})", i, shares[(i as usize) - 1].value());
+    }
+    println!("");
+
+    let parties: [u64; 3] = [1, 4, 3];
+    println!("If parties {:?} gather togheter, they can reveal the secret.", parties);
+
+    let inter = Polynomial::interpolate(
+        &parties.map(|i| FE::new(i)),
+        &parties.map(|i| shares[(i as usize) - 1])
+    ).unwrap();
+
+    let revealed_secret = inter.coefficients[0].value();
+
+    println!("Revealed secret is: {:?}", revealed_secret);
 }

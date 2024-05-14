9e789238ec3fa35366fa078269e206ecfd3dde97
fpresenza <francisco.presenza@gmail.com>
2024-05-13 22:55:18 -0300

    rsa: use UnsignedInteger only and number_theory lib

--- tests/rsa/src/main.rs
+++ tests/rsa/src/main.rs
@@ -1,46 +1,138 @@
-use gcd::Gcd;
-use lambdaworks_math::field::fields::u64_prime_field::U64FieldElement;
+use num_primes::Generator;
+use lambdaworks_math::{traits::ByteConversion, unsigned_integer::element::UnsignedInteger};
+use number_theory::{power_mod, extended_euclidean_algorithm};
+
+const LIMB_SIZE_BIT: usize = 64;
+const LIMB_SIZE_BYTE: usize = 8;
+
+fn random_unsigned_integer<const NUM_LIMBS: usize>(bit_size: usize) -> UnsignedInteger<NUM_LIMBS> {
+    UnsignedInteger::<NUM_LIMBS>::from_hex(
+        &Generator::new_prime(bit_size).to_str_radix(16)
+    ).unwrap()
+}
+
+#[derive(Debug)]
+struct RSA<const NUM_LIMBS: usize> {
+    encryption_exp: UnsignedInteger<NUM_LIMBS>,
+    decryption_exp: UnsignedInteger<NUM_LIMBS>,
+    modulus: UnsignedInteger<NUM_LIMBS>
+}
+
+impl<const NUM_LIMBS: usize> RSA<NUM_LIMBS> {
+    fn new() -> Self {
+        let zero = UnsignedInteger::<NUM_LIMBS>::from_u64(0);
+        let one = UnsignedInteger::<NUM_LIMBS>::from_u64(1);
+        let bit_size = NUM_LIMBS * LIMB_SIZE_BIT / 4;
+
+        // generate two random primes of bitsize
+        //      NUM_LIMBS * LIMB_SIZE_BIT / 4 
+        // to prevent UnsignedInteger overflow.
+        let p = random_unsigned_integer::<NUM_LIMBS>(bit_size);
+        let q = random_unsigned_integer::<NUM_LIMBS>(bit_size);
+
+        let modulus = &p * &q;
+        let euler_phi = (&p - &one) * (&q - &one);
+
+        // check 65537 < euler_phi and the they have no common factors.
+        // Since 65537 is prime, it is sufficient to check that is not a
+        // multiple of 65537.
+        let encryption_exp = UnsignedInteger::<NUM_LIMBS>::from_u64(65537);
+        assert!(encryption_exp < euler_phi);
+        let (_, rem) = euler_phi.div_rem(&encryption_exp);
+        assert!(rem != zero);
+        
+        let (_, decryption_exp, _) = extended_euclidean_algorithm(
+            encryption_exp.clone(),
+            euler_phi.clone()
+        );
+
+        Self { 
+            encryption_exp,
+            decryption_exp,
+            modulus
+        }
+    }
+
+    fn encrypt(
+            &self,
+            plaintext_as_integer: UnsignedInteger<NUM_LIMBS>, 
+            encryption_exp: UnsignedInteger<NUM_LIMBS>,
+            modulus: &UnsignedInteger<NUM_LIMBS>
+        ) -> UnsignedInteger<NUM_LIMBS> {
+        assert!(plaintext_as_integer < *modulus);
+        power_mod(plaintext_as_integer, encryption_exp, modulus)
+    }
+
+    fn decrypt(
+            &self, 
+            cyphertext_as_integer: UnsignedInteger<NUM_LIMBS>, 
+        ) -> UnsignedInteger<NUM_LIMBS> {
+        power_mod(cyphertext_as_integer, self.decryption_exp, &self.modulus)
+    }
+}
 
 fn main() {
-    // Take two prime numbers in the range 2..2^32. 
-    // This primes are taken from SageMath, since I don't know
-    // which library use to generate random primes in Rust.
-    // Also, it is checked that (p-1) * (q-1) is relatively prime 
-    // to the encryption key.
-    // let p = 598991051_u64;
-    // let q = 2840026297_u64;
-    // MOD equals p * q
-    const MOD: u64 = 1701150336507668147;
-    type FEMOD = U64FieldElement<MOD>;
-    const PHI_MOD: u64 = 1701150333068650800;
-    type FEPHI = U64FieldElement<PHI_MOD>;
-    let phi_phi_mod = 388215687235276800_u64;
+    const NUM_LIMBS: usize = 8;
+    type RSA512 = RSA<NUM_LIMBS>;
 
-    // TODO: using carmichael's is suppossedly more eficient.
-
-    // Generate keys
-    // It was checked using SageMath that 65537 is coprime to PHI_MOD
-    let encryption_key = 65537_u64;
-    assert_eq!(Gcd::gcd(encryption_key, PHI_MOD), 1);
-    println!("Encryption key is: ({}, {})", encryption_key, MOD);
-
-    // Here I use the .pow() method instead of the .inv() method since only
-    // the former is correct: 
-    // since PHI_MOD is not prime, given a number e, its inverse, d, equals:
-    //      d = e^{phi(PHI_MOD) - 1} % PHI_MOD
-    // but
-    //      d != e^{PHI_MOD - 2} % PHI_MOD 
-    // as is implemented in the .inv() method.
-    let decryption_key = *FEPHI::new(encryption_key).pow(phi_phi_mod - 1).value();
-    println!("Decryption key is: ({}, {})", decryption_key, MOD);
+    println!("----------");
+    println!("Generating Alice's key.");
+    let alica_rsa = RSA512::new();
+    println!("Alice's key: {:?}", alica_rsa);
+    println!("----------");
+    println!("Generating Bob's key.");
+    let bob_rsa = RSA512::new();
+    println!("Bob's key: {:?}", alica_rsa);
 
     // encrypt messages
-    let plain_msg = 68353567629759241_u64;
-    println!("Plain message is: {}", plain_msg);
+    println!("----------");
+    // let message = 
+    let message = "Hi! This is a Secret Message.";
+    let max_bytes = LIMB_SIZE_BYTE * NUM_LIMBS;
+    assert!(message.len() <= max_bytes);
+    println!("Plain message from Alice to Bob: {:?}", message);
+    let mut padded_message = (0..max_bytes-message.len()).map(|_| "\x00").collect::<String>();
+    padded_message.push_str(message);
+    // println!("padded_message {:?}", padded_message);
+
+    let plaintext_as_bytes = padded_message.as_bytes();
+    // println!("Plaintext as bytes: {:?}", plaintext_as_bytes);
+
+    let plaintext_as_integer = UnsignedInteger::<NUM_LIMBS>::from_bytes_be(
+        &plaintext_as_bytes
+    ).unwrap();
+    // println!("Message as UnsigedInteger: {:?}", plaintext_as_integer);
+
+    let cyphertext_as_integer = alica_rsa.encrypt(
+        plaintext_as_integer,
+        bob_rsa.encryption_exp.clone(),
+        &bob_rsa.modulus
+    );
+    // println!("Encrypted message as UnsigedInteger: {:?}", cyphertext_as_integer);
     
-    let cypher_msg = *FEMOD::new(plain_msg).pow(encryption_key).value();
-    println!("Encrypted message is: {}", cypher_msg);
-    
-    let recov_msg = *FEMOD::new(cypher_msg).pow(decryption_key).value();
-    println!("Recovered message is {}", recov_msg);
+    let cyphertext_as_bytes = cyphertext_as_integer.to_bytes_be();
+    // println!("Encrypted message as bytes from Alice to Bob: {:?}", cyphertext_as_bytes);
+
+    println!(
+        "
+        ---------------------------->
+        Sending over insecure channel
+        ---------------------------->
+        "
+    );
+
+    let recovered_cyphertext_as_integer = UnsignedInteger::<NUM_LIMBS>::from_bytes_be(
+        &cyphertext_as_bytes  
+    ).unwrap();
+    // println!("Recovered Encrypted message as UnsigedInteger: {:?}", recovered_cyphertext_as_integer);
+
+    let recovered_plaintext_as_integer = bob_rsa.decrypt(recovered_cyphertext_as_integer);
+    // println!("Recovered message is {:?}", recovered_plaintext_as_integer);
+
+    let recovered_plaintext_as_bytes = recovered_plaintext_as_integer.to_bytes_be();
+    // println!("Recovered plaintext as bytes is {:?}", recovered_plaintext_as_bytes);
+
+    let recoverd_message = String::from_utf8(recovered_plaintext_as_bytes);
+    println!("Recovered message {:?}", recoverd_message.unwrap());
+
 }
